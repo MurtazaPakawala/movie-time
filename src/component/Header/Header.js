a3c9049e66711eacd3ecf7bcfe946bc44735eb62
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-23 18:38:55 +1000

    styling the header component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div>
+      <span className="header">Movie Time 🎬🎥</span>
+    </div>
+  );
+}
+
+export default Header;
