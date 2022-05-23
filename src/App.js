13dc5d69c1d7ec6d90741cd744d5164b77cf6328
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-24 00:39:04 +1000

    setting up the routes movies tvseries search trending

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+
+import { Container } from "@mui/system";
 import "./App.css";
 import SimpleBottomNavigation from "./component/BottomNav";
 import Header from "./component/Header/Header";
+import Trending from "./Pages/Trending";
+import Movies from "./Pages/Movies";
+import Search from "./Pages/Search";
+import TvSeries from "./Pages/TvSeries";
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <div className="app"></div>
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/trending" element={<Trending />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tvseries" element={<TvSeries />} />
+          </Routes>
+        </Container>
+      </div>
       <SimpleBottomNavigation />
-    </>
+    </Router>
   );
 }
 
---
+++ src/Pages/Trending.js
@@ -0,0 +1,9 @@
+const Trending = () => {
+  return (
+    <div>
+      <h1>this is the trending page</h1>
+    </div>
+  );
+};
+
+export default Trending;
