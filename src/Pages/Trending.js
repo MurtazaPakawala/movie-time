db9b5a6d5c8fc75461871bf526d84b153d215ed8
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-24 16:12:25 +1000

    getting the content for the trendin page setting the endpoints for the key

--- src/Pages/Trending.js
+++ src/Pages/Trending.js
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Trending = () => {
+  const [content, setContent] = useState([]);
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    console.log(data.results);
+  };
+  useEffect(() => {
+    fetchTrending();
+  }, [content]);
   return (
     <div>
       <h1>this is the trending page</h1>
